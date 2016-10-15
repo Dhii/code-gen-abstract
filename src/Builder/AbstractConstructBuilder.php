27693e13e3bd9885876f916d144c7d0ce2762fd3
Miguel Muscat <miguelmuscat93@gmail.com>
2016-10-15 14:43:47 +0200

    Added `AbstractConstructBuilder`

---
+++ src/Builder/AbstractConstructBuilder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dhii\CodeGen\Builder;
+
+use Dhii\Blueprint\AbstractBuilder;
+use Dhii\Blueprint\BlueprintInterface;
+use Dhii\CodeGen\Blueprint\ConstructBlueprintInterface;
+
+/**
+ * Description of AbstractConstructBuilder.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+abstract class AbstractConstructBuilder extends AbstractBuilder implements ConstructBuilderInterface
+{
+    /**
+     * The delegation container instance.
+     *
+     * @var ContainerInterface
+     */
+    protected $delegationContainer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDelegationContainer()
+    {
+        return $this->delegationContainer;
+    }
+
+    /**
+     * Sets the delegation container instance.
+     *
+     * @param ContainerInterface $delegationContainer The new delegation container instance.
+     *
+     * @return AbstractConstructDelegationBuilder This instance.
+     */
+    public function setDelegationContainer(ContainerInterface $delegationContainer)
+    {
+        $this->delegationContainer = $delegationContainer;
+
+        return $this;
+    }
+
+    /**
+     * Checks if the builder supports the given construct blueprint.
+     *
+     * @return bool True if this build supports the given blueprint, false if not.
+     */
+    abstract protected function _supports(ConstructBlueprintInterface $blueprint);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(BlueprintInterface $blueprint)
+    {
+        return $blueprint instanceof ConstructBlueprintInterface && $this->_supports($blueprint);
+    }
+}
